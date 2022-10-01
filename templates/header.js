988021955d15d6e6c1456c0d147f1e416cdd0abd
Jasmin Dedic <jdedic1999@gmail.com>
2022-10-01 14:56:27 +0200

    Created and styled shopping basket

--- templates/header.js
+++ templates/header.js
@@ -40,6 +40,9 @@ function darkLightMode() {
     let moonIcon = document.querySelector(".fa-moon");
     let hamburgerBars = document.querySelectorAll(".bar");
 
+    // Content elements
+    let restaurantLogo = document.querySelector(".restaurantLogo");
+
     // Toggle active class
     bodyElem.classList.toggle("active");
     header.classList.toggle("active");
@@ -47,19 +50,25 @@ function darkLightMode() {
     headerAdress.classList.toggle("active");
     moonIcon.classList.toggle("active");
 
+    // ForEach loop to iterate over all three bars and apply same toggle at the same time
     hamburgerBars.forEach(bar => {
         bar.classList.toggle("active");
     });
+
+    // Content element toggle
+    restaurantLogo.classList.toggle("active");
 }
 
 
 // Hamburger menu
 function toggleHamburger() {
     // Get elements needed to create hamburger menu functionality 
+    let header = document.querySelector(".header");
     let logoContainer = document.querySelector(".logoContainer");
     let headerAdressCon = document.querySelector(".headerAdress");
     let flagDarkLightContainer = document.querySelector(".flagDarkLightContainer");
 
+    header.classList.toggle("activeHeight");
     logoContainer.classList.toggle("active");
     headerAdressCon.classList.toggle("active");
     flagDarkLightContainer.classList.toggle("active");
